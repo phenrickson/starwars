af1eccd5878def93710fdd11429da720ff597ce5
Phil Henrickson <phil.henrickson@github.com>
2024-04-08 11:08:06 -0500

    changed sentiment to use sentimentr

--- sentiment.R
+++ sentiment.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(tidytext)
 library(ggthemes)
 library(textdata)
+library(sentimentr)
+library(ggrepel)
 
 # load and set ggplot theme
 source('theme-phil.R')
@@ -55,12 +57,10 @@ remove_stopwords = function(data,
 
 # add sentiment
 add_sentiment = function(data,
-                         sentiment = get_sentiments("afinn"),
-                         by = "word") {
+                         by = c('document', 'character', 'line_number')) {
         
         data |>
-                inner_join(sentiment,
-                           by = by)
+        sentiment_by(by = by)
 
 }
 
@@ -72,50 +72,39 @@ starwars =
         load_data() |>
         clean_data()
 
-# get dictionary of stopwords
-data("stop_words")
-
-# tokenize and remove stopwords
-starwars_tokenized = 
-        starwars |>
-        get_tokens() |>
-        remove_stopwords(stopwords = stop_words)
-        
-# add sentiment with afinn
+# use sentimentr
 starwars_sentiment = 
-        starwars_tokenized |>
-        add_sentiment()
+        starwars |>
+        get_sentences() |> 
+        add_sentiment() |>
+        as_tibble()
 
 # sentiment by movie line
 starwars_sentiment |>
         filter(document == 'a new hope') |>
-        count(document, character, line_number, index = line_number %/% 8, value) |>
-        arrange(index) |>
-        group_by(document, index) |>
-        summarize(value = sum(value),
-                  .groups = 'drop') |> 
-        ggplot(aes(x=index,
-                   y=value,
-                   fill = value))+
+        group_by(document,  index = line_number %/% 8) |>
+        summarize(sentiment = sum(ave_sentiment)) |>
+        ggplot(aes(x=index, y=sentiment, fill = sentiment))+
         geom_col()+
-        scale_fill_gradient2_tableau(limits=c(-5, 2), oob = scales::squish)+
+        scale_fill_gradient2_tableau(oob = scales::squish)+
         guides(fill = guide_colourbar(title = "sentiment",
                                       title.position = "top",
                                       barwidth=8,
                                       barheight=0.5))+
-        facet_wrap(document ~.)+
-        labs(y = "sentiment score",
-             x = "line number")
+        ylab("character")+
+        xlab("sentiment score")+
+        facet_wrap(document~.)
 
 # sentiment by character
 starwars_sentiment |>
-        filter(document == 'a new hope') |>
-        count(document, character, line_number, index = line_number %/% 8, value) %>%
-        group_by(document, character) %>%
-        summarize(sentiment = sum(value)) %>%
-        ggplot(., aes(y=reorder(character, sentiment), x=sentiment, fill = sentiment))+
+        filter(document == 'a new hope')  |>
+        group_by(document, character) |>
+        summarize(sentiment = sum(ave_sentiment),
+                  word_count = sum(word_count)) |>
+        slice_max(n = 40, order_by = word_count) |>
+        ggplot(aes(y=reorder(character, sentiment), x=sentiment, fill = sentiment))+
         geom_col()+
-        scale_fill_gradient2_tableau(limits=c(-5, 2), oob = scales::squish)+
+        scale_fill_gradient2_tableau(oob = scales::squish)+
         guides(fill = guide_colourbar(title = "sentiment",
                                       title.position = "top",
                                       barwidth=8,
