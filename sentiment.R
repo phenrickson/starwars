19ec48465cec95659bce8dc628d7346e5db37231
Phil Henrickson <phil.henrickson@github.com>
2024-04-08 11:04:40 -0500

    calculated sentiment by afinn

---
+++ sentiment.R
@@ -0,0 +1,125 @@
+# load packages
+library(tidyverse)
+library(tidytext)
+library(ggthemes)
+library(textdata)
+
+# load and set ggplot theme
+source('theme-phil.R')
+theme_set(
+        theme_phil()+
+                theme(strip.text.x = element_text(size = 12)
+                )
+)
+
+# functions
+# load data
+load_data = function(file = "data/starwars_text.csv", ...) {
+        
+        read_csv(file)
+        
+}
+
+# function to clean up some pieces with the data
+clean_data = function(data) {
+        
+        data |> 
+                mutate(episode = case_when(document == 'a new hope' ~ 'iv',
+                                           document == 'the empire strikes back' ~ 'v',
+                                           document == 'return of the jedi' ~ 'vi')) |>
+                mutate(character = case_when(character == 'BERU' ~ 'AUNT BERU',
+                                             TRUE ~ character)) |>
+                select(episode, everything())
+}
+
+# function to tokenize
+get_tokens = function(data, ...) {
+        
+        data |>
+                unnest_tokens(
+                        word,
+                        dialogue,
+                        ...)
+}
+
+# strip stopwords
+remove_stopwords = function(data, 
+                            stopwords,
+                            by = "word") {
+        
+        data |>
+                anti_join(stopwords,
+                          by = by)
+        
+}
+
+# add sentiment
+add_sentiment = function(data,
+                         sentiment = get_sentiments("afinn"),
+                         by = "word") {
+        
+        data |>
+                inner_join(sentiment,
+                           by = by)
+
+}
+
+# tokenize and remove stopwords
+data("stop_words")
+
+# read in starwars text
+starwars =
+        load_data() |>
+        clean_data()
+
+# get dictionary of stopwords
+data("stop_words")
+
+# tokenize and remove stopwords
+starwars_tokenized = 
+        starwars |>
+        get_tokens() |>
+        remove_stopwords(stopwords = stop_words)
+        
+# add sentiment with afinn
+starwars_sentiment = 
+        starwars_tokenized |>
+        add_sentiment()
+
+# sentiment by movie line
+starwars_sentiment |>
+        filter(document == 'a new hope') |>
+        count(document, character, line_number, index = line_number %/% 8, value) |>
+        arrange(index) |>
+        group_by(document, index) |>
+        summarize(value = sum(value),
+                  .groups = 'drop') |> 
+        ggplot(aes(x=index,
+                   y=value,
+                   fill = value))+
+        geom_col()+
+        scale_fill_gradient2_tableau(limits=c(-5, 2), oob = scales::squish)+
+        guides(fill = guide_colourbar(title = "sentiment",
+                                      title.position = "top",
+                                      barwidth=8,
+                                      barheight=0.5))+
+        facet_wrap(document ~.)+
+        labs(y = "sentiment score",
+             x = "line number")
+
+# sentiment by character
+starwars_sentiment |>
+        filter(document == 'a new hope') |>
+        count(document, character, line_number, index = line_number %/% 8, value) %>%
+        group_by(document, character) %>%
+        summarize(sentiment = sum(value)) %>%
+        ggplot(., aes(y=reorder(character, sentiment), x=sentiment, fill = sentiment))+
+        geom_col()+
+        scale_fill_gradient2_tableau(limits=c(-5, 2), oob = scales::squish)+
+        guides(fill = guide_colourbar(title = "sentiment",
+                                      title.position = "top",
+                                      barwidth=8,
+                                      barheight=0.5))+
+        ylab("character")+
+        xlab("sentiment score")+
+        facet_wrap(document~.)
